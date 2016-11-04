7bbf4861f4c43345b0ce9c140fec94f2b8f209a3
Youngtae <Youngtae@Youngtaeui-MacBook-Air.local>
2016-11-04 17:24:57 +0900

    카테고리에 해당하는 식당리스트를 가져오는 모델및 뷰 추가

--- src/com/mc/delivery/action/restaurantaction/CategoryOptionAction.java
+++ src/com/mc/delivery/action/restaurantaction/CategoryOptionAction.java
@@ -1,6 +1,7 @@
 package com.mc.delivery.action.restaurantaction;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.mc.delivery.action.Action;
 import com.mc.delivery.action.ActionHelper;
+import com.mc.delivery.dao.RestaurantDAO;
+import com.mc.delivery.vo.RestaurantVO;
 
 public class CategoryOptionAction implements Action {
 
@@ -15,10 +18,16 @@ public class CategoryOptionAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//리퀘스트객체에서 추가 파라미터를 가져와서 어떤 카테고로인지 판단한다.
 		String viewPath="";
+		RestaurantDAO dao = RestaurantDAO.getRestaurantDAO();
 		String option= request.getParameter("option");
+		List<RestaurantVO> voList= null;
 		if(option.equals("pizza")){
+			voList = dao.selectOption(option);
+			request.setAttribute("restaurantList", voList);
 			viewPath="pizzaCategory.jsp";
 		}else if(option.equals("chicken")){
+			voList = dao.selectOption(option);
+			request.setAttribute("restaurantList", voList);
 			viewPath="chickenCategory.jsp";
 		}
 		
--- src/com/mc/delivery/dao/RestaurantDAO.java
+++ src/com/mc/delivery/dao/RestaurantDAO.java
@@ -83,5 +83,38 @@ public class RestaurantDAO {
 		 
 	}
 	
+	// 선택한 카테고리에 맞는 식당 리스트들 가져오기
+	public List<RestaurantVO> selectOption(String option){
+		List<RestaurantVO> voList = new ArrayList<>();
+		 Connection con = DBHelper.makeConnection();
+		 PreparedStatement pstmt =null;
+		 ResultSet rs= null;
+		 String sql="SELECT a.restaurant_name,a.location_name FROM "
+				 +"(SELECT * FROM restaurants as r LEFT JOIN locations as l "
+				 + "ON r.restaurant_location=l.location_id "
+				 +"WHERE restaurant_category= "
+				 + "(SELECT category_id FROM category WHERE category_name=?))AS a;";
+		 
+		 
+		 try {
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, option);
+			rs=pstmt.executeQuery();
+			while(rs.next()){
+				RestaurantVO vo = new RestaurantVO();
+				vo.setRestaurantName(rs.getString(1));
+				vo.setRestaurantLocation(rs.getString(2));
+				voList.add(vo);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			DBHelper.close(rs);
+			DBHelper.close(pstmt);
+			DBHelper.close(con);
+		}return voList;
+		 
+	}
 	
 }
