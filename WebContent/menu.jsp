5a8f50a7b3193790b9e0305cc1c0690dad359eef
AsagiriTatsuya <RedKane12@hotmail.com>
2016-11-21 15:09:02 +0900

    InsertMenuAction 추가

---
+++ WebContent/menu.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>menu.jsp</title>
+</head>
+<body>
+
+
+</body>
+</html>
---
+++ src/com/mc/delivery/action/restaurantaction/InsertMenuAction.java
@@ -0,0 +1,62 @@
+package com.mc.delivery.action.restaurantaction;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mc.delivery.action.Action;
+import com.mc.delivery.action.ActionHelper;
+import com.mc.delivery.dao.MenuDAO;
+import com.mc.delivery.databinding.DataBinding;
+import com.mc.delivery.vo.MenuVO;
+import com.oreilly.servlet.MultipartRequest;
+
+public class InsertMenuAction implements Action,DataBinding {
+	
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		MultipartRequest multi = (MultipartRequest) request.getAttribute("multi");
+		
+		String menuName = multi.getParameter("menuName");
+		String menuCategory = multi.getParameter("menuCategory");
+		String menuInfo= multi.getParameter("menuInfo");
+		
+		System.out.println(menuName);
+		System.out.println(menuCategory);
+		System.out.println(menuInfo);
+		
+		String menuImageFileName = "default_img.png";
+		
+		if(multi.getFilesystemName("menuImg") == null || multi.getFilesystemName("menuImg").equals(""))
+		{	
+		}
+		else
+		{
+			menuImageFileName = multi.getFilesystemName("menuImg");
+		}
+		
+		MenuVO vo = new MenuVO();
+		vo.setMenuName(menuName);
+		vo.setMenuCategory(menuCategory);
+		vo.setMenuInfo(menuInfo);
+		
+		ServletContext sc = request.getServletContext();
+		sc.getAttribute("menuDAO");
+		MenuDAO dao = (MenuDAO) sc.getAttribute("menuDAO");
+		
+		int result = dao.insertMenuInfo(vo);
+		request.setAttribute("result", result);
+		String viewPath = "result.jsp"; //result.jsp는 임시
+		ActionHelper.getActionHelper().actionFoward(request, response, viewPath);
+	}
+	
+	@Override
+	public Object[] getDataBinders()
+	{
+		return new Object[]{"menuVO", com.mc.delivery.vo.MenuVO.class};
+	}
+}
