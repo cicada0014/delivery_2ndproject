e26010887db36f28d1d4a9db1a286e7dd611deb3
noarcho <hansol2705@naver.com>
2016-12-01 15:40:59 +0900

    검색값 인코딩 문제 해결

--- WebContent/SearchForm.jsp
+++ WebContent/SearchForm.jsp
@@ -38,24 +38,6 @@
 </head>
 <body>
 	<div class="row ">
-		<div class="col s2 m2 l2"> </div>
-		<div class="row col s3 m3 l3">
-			
-			<form class="" action="" method="post">
-<!-- 				<i class="material-icons prefix">my_location</i>  -->
-			<div class="col s7 m7 l7">
-				<input
-					id="newlocation" type="text" class="validate " name="newlocation">
-				<label for="newlocation">새로운 위치지정</label>
-			</div>
-				<div class="col s2 m2 l2">
-				 <a id="resetLocationBtn"
-					class="waves-effect waves-light btn  "> <i
-					class="material-icons left">cloud</i>
-				</a>
-				</div>
-			</form>
-		</div>
 		<div class="col s4 m4 l4 center ">
 			<nav>
 				<div class="nav-wrapper ">
--- src/com/mc/delivery/action/restaurantaction/TakeListAction.java
+++ src/com/mc/delivery/action/restaurantaction/TakeListAction.java
@@ -38,9 +38,11 @@ public class TakeListAction implements Action{
 					System.out.println("옵션값 안들어옴");
 					String search = request.getParameter("search");
 					System.out.println(search);
+					String result = new String(search.getBytes("8859_1"), "UTF-8");
+					System.out.println("result : " + result);
 					List<CategoryVO> categoryList = (List<CategoryVO>)request.getSession().getAttribute("categoryListData");
-							voList = dao.selectSearchName(search,sessionLocation);
-							request.setAttribute("resultSize", dao.selectOptionCount(search, sessionLocation));
+							voList = dao.selectSearchName(result,sessionLocation);
+							request.setAttribute("resultSize", dao.selectOptionCount(result, sessionLocation));
 							
 							request.setAttribute("restaurantList", voList);
 							request.setAttribute("category", voList.get(0).getRestaurantCategory());
